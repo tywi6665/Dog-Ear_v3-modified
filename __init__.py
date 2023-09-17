ad9bf991b380ac086381e9c1a5f0c74b49aab74a
Tyler <tyletcole@gmail.com>
2023-09-17 16:28:46 -0600

    Getting scrapyd to work on heroku

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import io
+import os
+
+# https://devcenter.heroku.com/articles/runtime-principles#web-servers
+# The port to bind to is assigned by Heroku as the PORT environment variable.
+PORT = os.environ['PORT']
+with io.open("scrapyd.conf", 'r+', encoding='utf-8') as f:
+    f.read()
+    f.write(u'\nhttp_port = %s\n' % PORT)
